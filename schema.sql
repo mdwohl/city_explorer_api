bfb4dd2ae854951708f65f4f30dd423ffec98a60
mdwohl <mdwohl@gmail.com>
2020-08-20 16:12:44 -0700

    sql errors

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS table_locations;
 CREATE TABLE table_locations (
 id SERIAL PRIMARY KEY,
 search_query VARCHAR(255),
-formatted_query VARCHAR(255)
-latitude VARCHAR(255),
-longitude VARCHAR(255),
+formatted_query VARCHAR(255),
+latitude DECIMAL,
+longitude DECIMAL,
 );
